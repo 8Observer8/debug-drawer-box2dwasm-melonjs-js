f463dc99d9ac5284a71286ac25238008b33def1e
Ivan Enzhaev <8observer8@gmail.com>
2023-07-20 21:36:19 +0400

    Delete option for render.com hosting

--- src/init-box2d.js
+++ src/init-box2d.js
@@ -4,14 +4,7 @@ export let box2d = null;
 
 export function initBox2D(localhost = true) {
     return new Promise(resolve => {
-        Box2DLib({
-            locateFile: (url, scriptDirectory) => {
-                if (url.endsWith(".wasm") && !localhost) {
-                    return "file:///opt/render/project/src/node_modules/box2d-wasm/dist/umd/Box2D.simd.wasm";
-                }
-                return scriptDirectory + url;
-            }
-        }).then((re) => {
+        Box2DLib().then((re) => {
             box2d = re;
             resolve();
         });
